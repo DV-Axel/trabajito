c72f7191a77e43f951c7c18c7a3689638dfe9510
DV-Axel <axel.cichello@davinci.edu.ar>
2025-08-24 21:59:33 -0300

    Revert "test clone"
    This reverts commit 926922a8

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,12 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import Header from './components/Header.jsx';
-import Footer from './components/Footer.jsx';
-
-import Home from './pages/Home.jsx';
-import Login from './pages/Login.jsx';
-import Signup from "./pages/Signup.jsx";
+import './App.css'
 
 function App() {
+
     return (
-        <Router>
-            <Header />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/registro" element={<Signup />} />
-            </Routes>
-            <Footer />
-        </Router>
-    );
+        <h1 className="text-3xl font-bold underline">
+            Hello world
+        </h1>
+    )
 }
 
-export default App;
+export default App
